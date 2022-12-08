c41af4b9cc14f4ecdb48c01820c00b05465cb48b
Abel-D <abeldebebe818@gmail.com>
2022-12-08 21:47:06 +0300

    documentation fix

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,11 @@
 #include <stdio.h>
 /**
-* this program outputs a statement
-*/
+ * main - prints text as it is by escaping the first quotation
+ * mark. and as requirement detects,
+ * Return: ends with returning 0.Hope it passes the stupid linter
+ */
 int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle\n");
+	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
