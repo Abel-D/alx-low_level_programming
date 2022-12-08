07f8996192a90dec1530a12e2f42f6580d50c1fd
Abel-D <abeldebebe818@gmail.com>
2022-12-08 23:27:25 +0300

    list type size code added

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,14 @@
+# include <stdio.h>
+/**
+ * main - prints c datatypes memory size
+ *
+ * Return: 0 if success
+ */
+int main(void)
+{
+	printf("\n", "Size of a char: ", sizeof(char));
+	printf("\n", "Size of an int: ", sizeof(int));
+	printf("\n", "Size of a long int: "," sizeof(int));
+	printf("\n", "Size of a long long int: ", sizeof(long long int));
+	printf("\n", "Size of a float: ", sizeof(float));
+}
