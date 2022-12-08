abda91ed9e306d8bfec1679fc223bda70f41dc99
Abel-D <abeldebebe818@gmail.com>
2022-12-08 20:22:30 +0300

    fix

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,5 +1,7 @@
 #include <stdio.h>
-/** this program outputs a statement */
+/** 
+* this program outputs a statement
+*/
 int main(void)
 {
 	puts("Programming is like building a multilingual puzzle\n");
