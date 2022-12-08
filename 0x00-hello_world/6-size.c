3e44ec0929f427bc5ec37836a08b9e307e0431f3
Abel-D <abeldebebe818@gmail.com>
2022-12-08 23:48:09 +0300

    fix

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -7,13 +7,13 @@
 int main(void)
 {
 	int i;
-    	float f;
-    	long l;
+	float f;
+	long l;
 	long long ll;
-    	char c;
-	printf("Size of char: %zu bytes\n",  sizeof(c));
-	printf("Size of int: %zu bytes\n",  sizeof(i));
-	printf("Size of long: %zu bytes\n",  sizeof(l));
-	printf("Size of long long: %zu bytes\n",  sizeof(ll));
-	printf("Size of int: %zu bytes\n",  sizeof(f));
+	char c;
+	printf("Size of char: %zu bytes\n", sizeof(c));
+	printf("Size of int: %zu bytes\n", sizeof(i));
+	printf("Size of long: %zu bytes\n", sizeof(l));
+	printf("Size of long long: %zu bytes\n", sizeof(ll));
+	printf("Size of int: %zu bytes\n", sizeof(f));
 }
