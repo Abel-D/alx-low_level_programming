edec9e94d00c3701f4377fdf189e87918d4bd1b6
Abel-D <abeldebebe818@gmail.com>
2022-12-09 20:04:38 +0300

    write 0-9 added

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - print nos to 9
+ *
+ * Return: 0 if success
+ */
+int main(void)
+{
+	int y = 0;
+
+	while (y < 10)
+	{
+		printf("%d", y);
+		++y;
+	}
+	return (0);
+}
