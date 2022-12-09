f05928e5af04e203785e9084dfb8292e554708b8
Abel-D <abeldebebe818@gmail.com>
2022-12-09 18:40:13 +0300

    added check no program

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -1,5 +1,6 @@
 #include <stdlib.h>
 #include <time.h>
+#include <stdio.h>
 /**
  * main - checks no if greater than o and less than 6
  *
@@ -13,14 +14,14 @@ int main(void)
 	n = rand() - RAND_MAX / 2;
 	printf("%s", "Last digit of ");
 	printf("%d", n);
-	printf("%s", " is");
+	printf("%s", " is ");
 	x = n % 10;
 	printf("%d", x);
-	if(x > 5)
+	if (abs(x) > 5)
 		printf("%s", " and is greater than 5\n");
-	else if(x = 0)
-		printf("%s", " is zero\n");
-	else if(x > 0 && x < 6)
+	else if (abs(x) > 0 && abs(x) < 6)
 		printf("%s", " and is less than 6 and not 0\n");
+	else if (x == 0)
+		printf("%s", " is zero\n");
 	return (0);
 }
