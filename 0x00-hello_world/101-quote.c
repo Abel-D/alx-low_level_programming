f77de0a15701e34515863474dd1d668d1abf32a0
Abel-D <abeldebebe818@gmail.com>
2022-12-09 14:52:54 +0300

    added intel script and c- print to stderr

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,14 @@
+# include <stdio.h>
+/**
+  * main - writes output to stderr
+  *
+  * Return: return 0
+  */
+int main(void)
+{
+/**
+  * writes the following script to standard error output
+  */
+
+	fprintf(stderr,"and that piece of art is useful - Dona Korpar,2015-10-19\n");
+}
