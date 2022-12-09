7695e8b973c68724d738b9b6e52d93a46f990a87
Abel-D <abeldebebe818@gmail.com>
2022-12-09 15:11:43 +0300

    fixes

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,14 +1,12 @@
 # include <stdio.h>
 /**
-  * main - writes output to stderr
-  *
-  * Return: return 0
-  */
+ * main - writes output to stderr
+ * @stderr: err output
+ *
+ * Return: return 0
+ */
 int main(void)
 {
-/**
-  * writes the following script to standard error output
-  */
-
-	fprintf(stderr,"and that piece of art is useful - Dona Korpar,2015-10-19\n");
+	fprintf(stderr, "and that piece of art is useful - Dona Korpar,2015-10-19\n");
+	return (0);
 }
