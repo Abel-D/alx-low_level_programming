d2128a52de68c0b77da6e503d035ec828951b156
root <root@DESKTOP-24N9QBU.localdomain>
2022-12-10 19:20:11 +0300

    fixes

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -8,18 +8,22 @@ int print_int(int i);
 int main(void)
 {
 	int x = 123456789;
+
 	putchar('0');
 	print_int(x);
 	return (0);
 }
 
 /**
- * print - print int using putchar
+ * print_int - print int using putchar
+ * @n: integer to parse
+ *
+ * Return: 0 if success
  */
 int print_int(int n)
 {
-	if (n/10)
+	if (n / 10)
 		print_int(n / 10);
-	putchar(n%10 + '0');
+	putchar(n % 10 + '0');
 	return (0);
 }
