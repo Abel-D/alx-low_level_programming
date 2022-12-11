0d7731c05480eb41c9ddbaf468883e310afebeb2
root <root@DESKTOP-24N9QBU.localdomain>
2022-12-11 20:18:26 +0300

    added_3_combo_code

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,46 @@
+#include <stdio.h>
+/**
+ * main - list digits 0-9
+ *
+ * Return: 0 if success
+ */
+int main(void)
+{
+	int i, j, k;
+
+	for (i = 0; i < 10; i++)
+	{
+		for (j = 0; j <= 9; j++)
+		{
+			for (k = 0; k <= 9; k++)
+			{
+				if (i < j && j < k && i != j && j != k)
+				{
+					if (i / 10)
+						continue;
+					putchar(i % 10 + '0');
+					if (j / 10)
+						continue;
+					putchar(j % 10 + '0');
+					if (k / 10)
+                                                continue;
+                                        putchar(k % 10 + '0');		
+					if (i == 7)
+					{
+						if (j == 8)
+						{
+							if (k == 9)
+							{
+								break;
+							}
+						}
+					}
+				putchar(',');
+				putchar(' ');
+				}
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
