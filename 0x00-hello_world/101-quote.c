037bffc3e6a0a0974af6b65d23de078add6de266
root <root@DESKTOP-24N9QBU.localdomain>
2022-12-11 18:46:09 +0300

    fix

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,12 +1,12 @@
 # include <stdio.h>
 /**
- * main - writes output to stderr
- * @stderr: err output
+ * main - writes output to error output
  *
  * Return: return 1
  */
 int main(void)
 {
-	fprintf(stderr, "and that piece of art is useful\" - Dona Korpar, 2015-10-19\n");
+	fprintf(stderr, "and that piece of art is useful\"");
+	fprintf(stderr, " - Dona Korpar, 2015-10-19\n");
 	return (1);
 }
