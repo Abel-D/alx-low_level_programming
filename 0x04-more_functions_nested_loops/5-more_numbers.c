b2f9638b91a2a4d069b81fcb2597b0182eed6216
root <root@DESKTOP-24N9QBU.localdomain>
2022-12-16 20:17:58 +0300

    fix

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -8,11 +8,14 @@ void more_numbers(void)
 
 	for (i = 0; i < 10; i++)
 	{
-		for (j = 0; j < 10; j++)
+		for (j = 0; j < 14; j++)
 		{
 			if (j / 10)
 				continue;
-			putchar(j + '0');
+			if (j <= 9)
+				putchar(j + '0');
+			else
+				putchar(j % 10 + '0');
 		}
 		j = 0;
 		putchar('\n');
--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -8,7 +8,7 @@
  */
 void print_line(int n)
 {
-	int i;
+	int i = 0;
 
 	while (i < n)
 	{
