a27486f25687463771fbd6899816fc1107d1f42d
root <root@DESKTOP-24N9QBU.localdomain>
2022-12-19 18:41:52 +0300

    print half string code added

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ * puts_half - print half of a string
+ * @str: input string
+ */
+void puts_half(char *str)
+{
+	int half, i;
+	char *p;
+	int len = sizeof(str);
+
+	p = str;
+	if (len % 2 != 0)
+		half = (len - 1) / 2;
+	else
+		half = len / 2;
+	for (i = half + 1; p[i] != '\0'; i++)
+	{
+		putchar(p[i] + '\0');
+	}
+}
