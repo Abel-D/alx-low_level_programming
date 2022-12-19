e1d22bde2902d56cffbf15f895901f8042c9129f
root <root@DESKTOP-24N9QBU.localdomain>
2022-12-19 17:26:28 +0300

    string length code added

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <string.h>
+/**
+ * _strlen - calculate string length
+ * @s: input string
+ *
+ * Return: length integer
+ */
+int _strlen(char *s)
+{
+	int len;
+
+	len = strlen(s);
+	return (len);
+}
