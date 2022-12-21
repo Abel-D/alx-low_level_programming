720beb3c6b8ade4d9311bdc96221b6f5524f29ca
root <root@DESKTOP-24N9QBU.localdomain>
2022-12-21 17:12:12 +0300

    added strcmp

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,15 @@
+#include <string.h>
+/**
+ * _strcmp - compare strings
+ * @s1: str 1
+ * @s2: str 2
+ *
+ * Return: <0 if s1 less than s2, 0 if equal, 1 otherwise
+ */
+int _strcmp(char *s1, char *s2)
+{
+	int res;
+
+	res = strcmp(s1, s2);
+	return (res);
+}
---
+++ 0x06-pointers_arrays_strings/strcmp.c
@@ -0,0 +1,15 @@
+#include <string.h>
+/**
+ * _strcmp - compare string
+ * @s1: str 1
+ * @s2: str 2
+ *
+ * Return: integer
+ */
+int _strcmp(char *s1, char *s2)
+{
+	int res;
+
+	res = strcmp(s1, s2);
+	return (res);
+}
