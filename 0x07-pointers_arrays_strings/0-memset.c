548361f20d72afa21c72ee5775b8e50b8687f014
root <root@DESKTOP-24N9QBU.localdomain>
2023-01-02 22:04:47 +0300

    populate memory

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,26 @@
+#include <stdlib.h>
+/**
+ * _memset - populate memory with const value
+ * @s: memory location
+ * @b: constant value
+ * @n: no of bytes
+ *
+ * Return: pointer to memory area
+ */
+char *_memset(char *s, char b, unsigned int n)
+{
+	int i = 0;
+
+	while (abs(i) < n)
+	{
+		*s++ = b;
+		i++;
+	}
+	i = n;
+	while (i > 0)
+	{
+		s--;
+	}
+	return (s);
+}
+
---
+++ 0x07-pointers_arrays_strings/README.md
@@ -0,0 +1,1 @@
+this is the first project of 2023 on pointer and arrays
