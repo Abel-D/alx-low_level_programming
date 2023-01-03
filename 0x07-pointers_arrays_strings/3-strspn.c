4eee8bbe9ea51886e9492fbddd7392d122bb62e1
Abel-D <abeldebebe818@gmail.com>
2023-01-03 19:23:33 +0300

    added stspn

---
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+/**
+ * unsigned int _strspn - return length of substring
+ * @s: input text
+ * @accept: subscript of text
+ *
+ * Return: length of substring in memory
+ */
+unsigned int _strspn(char *s, char *accept)
+{
+	int length;
+	length = strspn(s, accept);
+
+	return (abs(length));
+}
