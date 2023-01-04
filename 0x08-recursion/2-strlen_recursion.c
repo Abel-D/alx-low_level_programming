c84572e62f42d2a0a9711ea48ccc3578e6af99fd
Abel-D <abeldebebe818@gmail.com>
2023-01-04 14:36:21 +0300

    strlen added

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * _strlen_recursion - string length
+ * @s: string input
+ *
+ * Return: length of string
+ */
+int _strlen_recursion(char *s)
+{
+	int length;
+
+	length = strlen(s);
+	return (length);
+}
