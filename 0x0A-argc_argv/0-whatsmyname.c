85cc224eae29bb3c653460905adb38584474c71f
Abel-D <abeldebebe818@gmail.com>
2023-01-10 16:50:45 +0300

    added show prog name code

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,20 @@
+#include <stdlib.h>
+#include <stdio.h>
+/**
+ * main - print program name
+ * @argc: no of params
+ * @argv: prog name
+ *
+ * Return: 0 if success
+ */
+int main (int argc, char *argv[])
+{
+	int i = 1;
+
+	while (i < argc)
+	{
+	printf("%s\n", *argv);
+	i++;
+	}
+	return (0);
+}
