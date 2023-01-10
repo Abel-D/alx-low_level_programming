cecf95233c31432e5c3673acc846d33d89f9f66b
Abel-D <abeldebebe818@gmail.com>
2023-01-10 16:57:09 +0300

    fix

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -7,11 +7,11 @@
  *
  * Return: 0 if success
  */
-int main (int argc, char *argv[])
+int main(int argc, char *argv[])
 {
 	int i = 0;
 
-	while(i < argc)
+	while (i < argc)
 	{
 	printf("%s\n", *argv);
 	i++;
