9840730ce6040f1f3fe397370914cfdb056839d5
Abel-D <abeldebebe818@gmail.com>
2023-01-11 12:29:18 +0300

    added first challenge

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * create_array - create array and fill with constant byte
+ * @size: size of array
+ * @c: const char val
+ *
+ * Return: char ptr
+ */
+char *create_array(unsigned int size, char c)
+{
+	char *p = (char *)malloc(size * sizeof(char));
+	unsigned int i = 0;
+
+	while (i < size)
+	{
+		*p = c;
+		i++;
+	}
+	return (p);
+}
