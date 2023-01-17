2643a17b0f23c37b10c499b881354ae2cd9ed05b
Abel-D <abeldebebe818@gmail.com>
2023-01-17 14:52:07 +0300

    added new type dog_t

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -8,10 +8,23 @@
  */
 typedef struct dog
 {
-	char *name;
-	float age;
-	char *owner;
+        char *name;
+        float age;
+        char *owner;
 } dog;
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
+
+/**
+ * struct dog - definition of dog type
+ * @name: name of the dog
+ * @age: age of the dog
+ * @owner: owner of the dog
+ */
+typedef struct dog_t
+{
+        char *name;
+        float age;
+        char *owner;
+} dog_t;
 #endif
