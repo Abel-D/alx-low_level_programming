f941376baf8eb2e480a742936e049c58407c4e53
Abel-D <abeldebebe818@gmail.com>
2023-01-18 17:30:31 +0300

    fix

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -12,20 +12,16 @@ int int_index(int *array, int size, int (*cmp)(int))
 {
 	int res, i = 0;
 
+	if (size <= 0)
+	{
+		res = -1;
+	}
 	while (i < size)
 	{
-		if (size <= 0)
+		if ((*cmp)(array[i]))
 		{
-			res = -1;
+			res = i;
 			break;
-		}
-		else
-		{
-			if ((*cmp)(array[i]))
-			{
-				res = i;
-				break;
-			}
 		}
 		i++;
 	}
